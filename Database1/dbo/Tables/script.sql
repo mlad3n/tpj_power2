580c08ea803cea55cd841f42e393e65e1bebe4fa
mladen <mladen@DESKTOP>
2016-10-16 14:06:50 +0200

    Added proper database project

---
+++ Database1/dbo/Tables/script.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[script]
+(
+    [report] NVARCHAR(50) NOT NULL, 
+    [chapter] INT NOT NULL, 
+    [source] INT NOT NULL, 
+	[source_path] NVARCHAR(MAX) NOT NULL, 
+    PRIMARY KEY ([source], [report], [chapter])
+)
